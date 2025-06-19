1893035bc6e40268a4dbdf7fdf9534fcd8e3056f
Gene <elkaufman5@gmail.com>
2025-06-18 17:14:33 -0700

    Fix yes selection for dungeon wagon access prompt

--- Assets/Scripts/Game/PlayerActivate.cs
+++ Assets/Scripts/Game/PlayerActivate.cs
@@ -1132,6 +1132,7 @@ namespace DaggerfallWorkshop.Game
         // Access wagon or dungeon exit
         private void DungeonWagonAccess_OnButtonClick(DaggerfallMessageBox sender, DaggerfallMessageBox.MessageBoxButtons messageBoxButton)
         {
+            sender.CloseWindow();
             if (messageBoxButton == DaggerfallMessageBox.MessageBoxButtons.No)
             {
                 playerEnterExit.TransitionDungeonExterior(true);
@@ -1142,7 +1143,6 @@ namespace DaggerfallWorkshop.Game
                 DaggerfallUI.Instance.InventoryWindow.AllowDungeonWagonAccess();
                 uiManager.PushWindow(DaggerfallUI.Instance.InventoryWindow);
             }
-            sender.CloseWindow();
         }
 
         // Look for building array on object, then on direct parent
